9469a8929db4b92ca37edf0ec81919b21ce4ab72
binhxcz <superping36@gmail.com>
2026-05-06 19:26:30 +0700

    update

--- pages/MyInforPage.ts
+++ pages/MyInforPage.ts
@@ -8,7 +8,7 @@ export class MyInfoPage {
     readonly id:Locator;
     readonly otherid:Locator;
     readonly driverlicensenumber:Locator;
-    readonly rlicenseexpirydate:Locator;
+    readonly driverlicenseexpirydate:Locator;
     readonly nationality:Locator;
     readonly maritalstatus:Locator;
     readonly dateofbirth:Locator;
@@ -16,11 +16,47 @@ export class MyInfoPage {
     readonly femalegender:Locator;
     readonly buttonsave1:Locator;
 
+    constructor(page:Page){
+        this.page = page;
+        this.firstname = page.locator(repo.getPersionalDetailInputFirstnameObject())
+        this.middlename = page.locator(repo.getPersionalDetailInputMiddlenameObject())
+        this.lastname = page.locator(repo.getPersionalDetailInputLastnameObject())
+        this.id = page.locator(repo.getPersionalDetailInputEmployeeidObject())
+        this.otherid = page.locator(repo.getPersionalDetailInputOtherIdObject())
+        this.driverlicensenumber = page.locator(repo.getPersionalDetailDriverLicensedObject())
+        this.driverlicenseexpirydate = page.locator(repo.getPersionalDetailDriverLicensedExdateObject())\
+        this.nationality = page.locator(repo.getPersionalDetailNationalyObject())
+        this.maritalstatus = page.locator(repo.getPersionalDetaiMaritalstatusObject())
+        this.dateofbirth = page.locator(repo.getPersionalDetaiDateOfBirthObject())
+        this.malegender = page.locator(repo.getPersionalDetaiGenderMaleObject())
+        this.femalegender = page.locator(repo.getPersionalDetaiGenderFemaleObject())
+        this.buttonsave1 = page.locator(repo.getPersionalDetaiButtonSave1Object())
+    }
 
-
-
-
-
-
+    async enterFirstName(firstname:string){
+        await this.firstname.fill(firstname)
+    }
+    async enterMiddleName(middlename:string){
+        await this.firstname.fill(middlename)
+    }
+    async enterLastName(lastname:string){
+        await this.lastname.fill(lastname)
+    }
+    async enterEmployeeId(empployeeid:string){
+        await this.id.fill(empployeeid)
+    }
+    async enterOtherId(otherid:string){
+        await this.otherid.fill(otherid)
+    }
+     async enterDriveLicenseNumber(drivernumber:string){
+        await this.driverlicensenumber.fill(drivernumber)
+    }
+    async enterDriverLicenseExpiryDate(date: string) {
+        await this.driverlicenseexpirydate.fill(date);
+    }
+    async enterDateOfBirth(date: string) {
+        await this.dateofbirth.fill(date);
+    }
+    
     
 }
